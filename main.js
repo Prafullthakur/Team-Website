7f079bafd60f9aa907196eaceea034e751e8576d
prafull <prafullchuahn45@gmail.com>
2020-06-01 22:59:53 +0530

    add

--- main.js
+++ main.js
@@ -1,1 +1,8 @@
-document.addEventListener('contextmenu', event => event.preventDefault());
+//document.addEventListener('contextmenu', event => event.preventDefault());
+var night = document.getElementsByClassName('.night');
+
+function myfunction() {
+    night.className = "night-mode";
+    console.log('working');
+}
+night.onclick = myfunction();
