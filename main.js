782ee1bd1cbbae3e6bf1ce11e1453b095c415788
prafull <prafullchuahn45@gmail.com>
2020-06-02 01:21:46 +0530

    add

--- main.js
+++ main.js
@@ -1,8 +1,12 @@
 //document.addEventListener('contextmenu', event => event.preventDefault());
 var night = document.querySelector('.night');
+var container = document.querySelector('.container');
 var count = 0;
 
 function myfunction() {
     count++;
-    if (count % 2 != 0) { night.className = "night-mode"; } else { night.className = "night"; }
+    if (count % 2 != 0) {
+        night.className = "night-mode";
+        container.style.background = 'rgba(0, 0, 0, 0.1)';
+    } else { night.className = "night"; }
 }
