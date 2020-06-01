94973bf4b67c340b98766690efad5b1f5da76ad9
prafull <prafullchuahn45@gmail.com>
2020-06-02 02:55:07 +0530

    add

--- main.js
+++ main.js
@@ -7,9 +7,9 @@ function myfunction() {
     count++;
     if (count % 2 != 0) {
         night.className = "night-mode";
-        container.style.background = 'rgba(0, 0, 0, 0.1)';
+
     } else {
         night.className = "night";
-        container.style.background = 'white';
+
     }
 }
