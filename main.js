3dd032f60064c6685fb62618b44df627e1127394
prafull <prafullchuahn45@gmail.com>
2020-06-02 16:33:11 +0530

    add

--- main.js
+++ main.js
@@ -1,6 +1,9 @@
 //document.addEventListener('contextmenu', event => event.preventDefault());
 var night = document.querySelector('.night');
 var container = document.querySelector('.container');
+var a1 = document.querySelector('.a1');
+var a2 = document.querySelector('.a2');
+var a3 = document.querySelector('.a3');
 var count = 0;
 
 function myfunction() {
@@ -12,4 +15,22 @@ function myfunction() {
         night.className = "night";
 
     }
+}
+
+function change1() {
+    a1.className = "achange";
+    a2.className = "a2";
+    a3.className = "a3";
+}
+
+function change2() {
+    a1.className = "a1";
+    a2.className = "achange";
+    a3.className = "a3";
+}
+
+function change3() {
+    a1.className = "a1";
+    a2.className = "a2";
+    a3.className = "achange";
 }
